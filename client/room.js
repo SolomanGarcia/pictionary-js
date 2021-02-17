33095fa5025faf6018cd0482cabb9618b4a3e4de
Soloman Garcia <soloman.garcia@gmail.com>
2021-02-17 11:42:52 -0700

    create endRound and hide functions

--- client/room.js
+++ client/room.js
@@ -10,3 +10,19 @@ const roomId = urlParams.get("room-id");
 if (!name || !roomId) window.location = "/index.html";
 
 const socket = io(serverURL);
+const guessForm = document.querySelector("[data-guess-form]");
+const guessInput = document.querySelector("[data-guess-input]");
+const wordElement = document.querySelector("[data-word]");
+const messageElement = document.querySelector("[data-messages]");
+const readyButton = document.querySelector("[data-ready-btn]");
+const canvas = document.querySelector("[data-canvas]");
+
+endRound();
+
+function endRound() {
+  hide(guessForm);
+}
+
+function hide(element) {
+  element.classList.add("hide");
+}
