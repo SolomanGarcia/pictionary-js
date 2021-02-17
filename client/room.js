723bb23caf4bd4cd9962854809a383b6063f3d7e
Soloman Garcia <soloman.garcia@gmail.com>
2021-02-17 11:34:16 -0700

    Setup rooms

--- client/room.js
+++ client/room.js
@@ -3,6 +3,10 @@ import { io } from "socket.io-client";
 const production = process.env.NODE_ENV === "production";
 const serverURL = production ? "realsite.com" : "http://localhost:3000";
 
-const socket = io(serverURL);
+const urlParams = new URLSearchParams(window.location.search);
+const name = urlParams.get("name");
+const roomId = urlParams.get("room-id");
 
-console.log(socket);
+if (!name || !roomId) window.location = "/index.html";
+
+const socket = io(serverURL);
